13542352d3be41d76d138e8215376225edfda8b9
Ribeiro <geovanny.ribeiro@gmail.com>
2016-02-16 10:21:25 -0300

    new refactoring

--- src/main/java/br/com/gribeiro/imex/chain/MacroHandler.java
+++ src/main/java/br/com/gribeiro/imex/chain/MacroHandler.java
@@ -1,11 +1,14 @@
 package br.com.gribeiro.imex.chain;
 
 import java.util.List;
+
 import br.com.gribeiro.imex.process.Request;
 
 public class MacroHandler extends AbstractHandler {
 	
 	private List<MicroHandler> microHandlers;
+	private Integer id;
+	private String name;
 
 	@Override
 	public Request handle(Request request) {
@@ -17,7 +20,9 @@ public class MacroHandler extends AbstractHandler {
 		return currentRequest;
 	}
 	
-	public MacroHandler(List<MicroHandler> microHandlers){
+	public MacroHandler(Integer id, String name, List<MicroHandler> microHandlers){
+		this.id = id;
+		this.name = name;
 		this.microHandlers = microHandlers;
 	}
 	
@@ -27,6 +32,14 @@ public class MacroHandler extends AbstractHandler {
 	
 	protected void remove(MicroHandler microHandler){
 		this.microHandlers.remove(microHandler);
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getName() {
+		return name;
 	}
 
 }
--- src/main/java/br/com/gribeiro/imex/main/MainTest.java
+++ src/main/java/br/com/gribeiro/imex/main/MainTest.java
@@ -2,10 +2,6 @@ package br.com.gribeiro.imex.main;
 
 import java.util.Arrays;
 import java.util.List;
-
-import br.com.gribeiro.imex.chain.ImportMacroHandlerOne;
-import br.com.gribeiro.imex.chain.ImportMacroHandlerThree;
-import br.com.gribeiro.imex.chain.ImportMacroHandlerTwo;
 import br.com.gribeiro.imex.chain.MacroHandler;
 import br.com.gribeiro.imex.chain.MicroHandler;
 import br.com.gribeiro.imex.chain.MicroHandlerA;
@@ -17,9 +13,9 @@ import br.com.gribeiro.imex.process.Request;
 public class MainTest {
 	
 	public static void main(String[] args) {
-		MacroHandler firstMacroHandler = new ImportMacroHandlerOne(Arrays.asList((MicroHandler)new MicroHandlerA()));
-		MacroHandler secondMacroHandler = new ImportMacroHandlerTwo(Arrays.asList((MicroHandler)new MicroHandlerB()));
-		MacroHandler thirdMacroHandler = new ImportMacroHandlerThree(Arrays.asList((MicroHandler)new MicroHandlerA() , (MicroHandler)new MicroHandlerC()));
+		MacroHandler firstMacroHandler = new MacroHandler(1, "firstMacroHandler", Arrays.asList((MicroHandler)new MicroHandlerA()));
+		MacroHandler secondMacroHandler = new MacroHandler(2, "secondMacroHandler",Arrays.asList((MicroHandler)new MicroHandlerB()));
+		MacroHandler thirdMacroHandler = new MacroHandler(3, "thirdMacroHandler",Arrays.asList((MicroHandler)new MicroHandlerA() , (MicroHandler)new MicroHandlerC()));
 		
 		
 		List<MacroHandler> macroHandlers = Arrays.asList(firstMacroHandler, secondMacroHandler, thirdMacroHandler);
